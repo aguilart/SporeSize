04d1f855ddb2426aba55299d213525062ce7ca9f
aguilart <calgit@gmail.com>
2017-04-25 16:26:59 +0200

    Organizing script

--- AMF_Taxonomy_Traits_Schlusser.R
+++ AMF_Taxonomy_Traits_Schlusser.R
@@ -1,24 +1,11 @@
 ###########Creating a complete database based on Schlusser website######################
 
+#loading packages
+library(tidyverse)
 
-##LOADING THE TAXONOMIC REFERENCE
-AMF_All<-read.csv("AMF_NamesTraitDataComplete.csv",header = TRUE,stringsAsFactors = FALSE)
+#loading functions
 
-#UDATING THE SPORE AREA COLUMN
-AMF_All$SporeArea<-
-  ((AMF_All$dim1.min+AMF_All$dim1.max)/4)*
-  ((AMF_All$dim2.min+AMF_All$dim2.max)/4)*
-  pi
-
-#CREATING A COPY OF THE TAXONOMIC REFERENCE AND REMOVING THE ORIGINAL FROM WORKSPACE
-AMF_All_Copy<-AMF_All
-rm(AMF_All)
-AMF_All_Copy[,1]<-sub("\\s","_",AMF_All_Copy[,1])
-
-#Creating a new dataframe just for AMF taxonomy, where each correct names gets repeated 
-#acccording the number of synonyms it has.
-
-## 1. Creating the function (Will Cornwell)
+#Creating the function "restructure" (Will Cornwell)
 restructure<-function(old){
   good.names<-NA
   Synonyms<-NA
@@ -41,8 +28,36 @@ restructure<-function(old){
   return(data.frame(good.names=good.names,Synonyms=Synonyms,stringsAsFactors = FALSE))
 }
 
+##Loading file and tidyig up
+AMF_All<-read.csv("AMF_NamesTraitDataComplete.csv",header = TRUE,stringsAsFactors = FALSE)
+          AMF_All$SporeArea<-#UDATING THE SPORE AREA COLUMN
+            ((AMF_All$dim1.min+AMF_All$dim1.max)/4)*
+            ((AMF_All$dim2.min+AMF_All$dim2.max)/4)*
+            pi
 
-## 2. Using "reestructure" to get a table of "accepted names" and "synonyms". 
-#Then adding underscore between genus and species name
+AMF_All_Copy<-AMF_All#CREATING A COPY
+              rm(AMF_All)
+              AMF_All_Copy[,1]<-sub("\\s","_",AMF_All_Copy[,1])
+
+#Creating a new dataframe just for AMF taxonomy, where each correct names gets repeated 
+#acccording the number of synonyms it has. In order to do this, Will created the function
+# "restructure" (see above)
 
 AMF_Taxonomy<-restructure(AMF_All_Copy)
+
+
+##Plotting some histograms for describing data variation
+
+qplot(log10(AMF_All_Copy$SporeArea),
+      #AMF_All_Copy$SporeArea,
+      geom="histogram",
+      #binwidth = 0.5,  
+      main = "Spore size (spore projected area) mycorrhizal fungi", 
+      xlab = "log10 Spore size",
+      ylab = "Number of species",
+      fill=I("blue"), 
+      col=I("black"), 
+      alpha=I(.2)#,xlim=c(20,50)
+      
+      )
+
