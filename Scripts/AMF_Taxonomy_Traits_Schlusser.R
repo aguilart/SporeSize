5ea8ca3deb968911bd49e6b98d0f60ef8fb09b83
aguilart <calgit@gmail.com>
2017-09-14 17:49:49 +0200

    updating names, adding names and cleaning following Schlusser website update 2017

--- Scripts/AMF_Taxonomy_Traits_Schlusser.R
+++ Scripts/AMF_Taxonomy_Traits_Schlusser.R
@@ -40,11 +40,98 @@ AMF_All_Copy<-AMF_All#CREATING A COPY
               AMF_All_Copy[,1]<-sub("\\s","_",AMF_All_Copy[,1])
               AMF_All_Copy[67,3]<-"Endogone_heterogama Gigaspora_heterogama Scutellospora_heterogama Fuscutata_heterogama"
 
+              AMF_All_Copy<-AMF_All_Copy[grep("_",AMF_All_Copy$good.names),]
+              AMF_All_Copy<-AMF_All_Copy[-c(163,164),]
+              
+              rownames(AMF_All_Copy)<-NULL
+              
+              
 #Updating and cleaning the dataset (September 2017)
 
-AMF_All_Copy<-AMF_All_Copy[grep("_",AMF_All_Copy$good.names),]
-
               
+schlusserUpdate<-read.csv("SchlusserWebSiteUpdate2017.csv",header = T, stringsAsFactors = F)  
+
+                schlusserUpdate$good.names<-sub(" ","_",schlusserUpdate$good.names)
+                schlusserUpdate$good.names<-sub("\\s","_",schlusserUpdate$good.names)
+                schlusserUpdate<-schlusserUpdate[grep("_",schlusserUpdate$good.names),]
+                
+                names(schlusserUpdate)[3]<-"Basionyms_Synonyms_Comments"
+                names(schlusserUpdate)[4]<-"Authorities"
+                names(schlusserUpdate)[5]<-"Family"
+                names(schlusserUpdate)[6]<-"Order"
+                
+                schlusserUpdate[80,3]<-"Basionym: Glomus compressum Sieverd., Oehl, Palenz., Sánchez-Castro & G.A. Silva"
+                rownames(schlusserUpdate)<-NULL
+
+                schlusserUpdate[215,1]<-"Racocetra_alborosea"
+                schlusserUpdate[207,1]<-"Racocetra_castanea"
+
+#Updating dataset using the updtade of Schlusser website from 2017
+
+old_names<-AMF_All_Copy[!AMF_All_Copy$good.names%in%schlusserUpdate$good.names,1]
+
+AMF_All_Copy[!AMF_All_Copy$good.names%in%schlusserUpdate$good.names,3]<-
+  paste(old_names,AMF_All_Copy[!AMF_All_Copy$good.names%in%schlusserUpdate$good.names,3])
+
+AMF_All_Copy[!AMF_All_Copy$good.names%in%schlusserUpdate$good.names,3]<-
+  sub(" NA$","",AMF_All_Copy[!AMF_All_Copy$good.names%in%schlusserUpdate$good.names,3])
+
+#changing some "old" to "new" accepted names according to the website update 2017
+AMF_All_Copy[!AMF_All_Copy$good.names%in%schlusserUpdate$good.names,c(1:3)]
+
+AMF_All_Copy[94,1]<-"Rhizophagus_aggregatus"
+AMF_All_Copy[99,1]<-"Diversispora_arenaria"
+AMF_All_Copy[101,1]<-"Dominikia_aurea"
+AMF_All_Copy[111,1]<-"Claroideoglomus_candidum"
+AMF_All_Copy[116,1]<-"Corymbiglomus_corymbiforme"
+AMF_All_Copy[129,1]<-"Corymbiglomus_globiferum"
+AMF_All_Copy[136,1]<-"Dominikia_indica"
+AMF_All_Copy[138,1]<-"Rhizophagus_invermaium"
+AMF_All_Copy[169,1]<-"Corymbiglomus_tortuosum"
+
+#Adding new species that have been introduced to in Schlusser Website
+
+new_species<-
+            schlusserUpdate[!schlusserUpdate$good.names%in%AMF_All_Copy$good.names,]
+            rownames(new_species)<-NULL
+          
+                        rownames(
+                        new_species[
+                        grep("nym",new_species$Basionyms_Synonyms_Comments),])
+
+            new_species$Synonyms<-NA
+            
+            new_species$Synonyms[c(11,15,24,25,28,30,33,36,37,38,39,40,41)]<-
+              c(NA,"Glomus_compressum","Glomus_bistratum","Glomus_perpusillum",
+              NA,NA,"Racocetra_intraornata","Glomus_macrocarpum","Glomus_macrocarpum",
+             "Glomus_macrocarpum",
+             "Glomus_fulvum","Glomus_fulvum",
+             "Glomus_fasciculatum")
+
+
+AMF_All_Copy<-merge(AMF_All_Copy,new_species[,c(1,3)],all = T)
+
+
+#deleting duplicated names
+
+AMF_All_Copy[grep("deserticola",AMF_All_Copy$good.names),c(1,2,13)]
+AMF_All_Copy[157,c(1,2)]<-AMF_All_Copy[290,c(1,2)]
+AMF_All_Copy[grep("heterogama",AMF_All_Copy$good.names),c(1,2,13)]
+AMF_All_Copy$Comments[79]<-"INVAM data; others: original description (Endogone heterogama) 150-202; Oehl version (Fuscutata heterogama): 165-280"
+AMF_All_Copy[grep("viscosum",AMF_All_Copy$good.names),c(1,2,13)]
+AMF_All_Copy[AMF_All_Copy$good.names=="Septoglomus_viscosum",2]<-"Glomus_viscosum Viscospora_viscosa"
+AMF_All_Copy[grep("punctata",AMF_All_Copy$good.names),c(1,2,13)]
+AMF_All_Copy<-AMF_All_Copy[-c(34,125,200,290),]
+rownames(AMF_All_Copy)<-NULL
+
+
+#Adding extra information from the website
+AMF_All_Copy<-left_join(AMF_All_Copy,schlusserUpdate,by ="good.names")
+              AMF_All_Copy<-AMF_All_Copy[c(1,2,28,31,32,4:27,30,3,29)]
+              
+rm(old_names,new_species,schlusserUpdate)
+
+
 #Creating a new dataframe just for AMF taxonomy, where each correct names gets repeated 
 #acccording the number of synonyms it has. In order to do this, Will created the function
 # "restructure" (see above)
